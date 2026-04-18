c026637d534331394a876669063a610716621053
vincent reddy <vincentreddy25@gmail.com>
2026-04-18 07:33:00 +0530

    feat: new task FormValidation

---
+++ _miniTask/FormValidation/data.js
@@ -0,0 +1,26 @@
+export const users = [
+    {
+        id: 1,
+        name: "vincent",
+        email: "vincent@gmail.com",
+        passkey: "riya"
+    },
+    {
+        id: 2,
+        name: "ashwin",
+        email: "ashwin@gmail.com",
+        passkey: "priya"
+    },
+    {
+        id: 3,
+        name: "charan",
+        email: "charan@gmail.com",
+        passkey: "laya"
+    },
+    {
+        id: 4,
+        name: "pavan",
+        email: "pavan@gmail.com",
+        passkey: "vaya"
+    },
+]
